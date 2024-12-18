2b02f96e80b6a82f5d7e06aecc48fb0040e3ffb6
Piyush Saini <134310734+piyushhsainii@users.noreply.github.com>
2024-12-19 02:37:55 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,11 @@ const nextConfig: NextConfig = {
       asyncWebAssembly: true, // Enable async WASM loading (recommended)
       syncWebAssembly: true, // Alternatively, enable sync loading if needed
     };
-
     // Ensure WebAssembly files are handled correctly
     config.module.rules.push({
       test: /\.wasm$/,
-      type: "webassembly/async", // Use 'webassembly/async' for async loading of WASM files
+      type: "webassembly/async", // Use 'webassembly/async' for async loading of WASM files,
+      exclude: /node_modules/,
     });
 
     return config;
